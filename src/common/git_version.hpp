7f77804ed058d76a8ac72dccbb0ac5aa116cd2d2
root <kamaljitsinghsikhamazon@gmail.com>
2020-11-04 18:05:29 +0000

    This is a new commit for what I originally planned to be amended

---
+++ src/common/git_version.hpp
@@ -0,0 +1,27 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef __COMMON_GIT_VERSION_HPP__
+#define __COMMON_GIT_VERSION_HPP__
+
+// This file contains artifacts gathered by an initial configuration
+// phase but never overwritten if populated already.
+
+#define BUILD_GIT_SHA "1fb36dcc5a0099f147cd01bd82cd7b4f0aec2256"
+
+#define BUILD_GIT_TAG "1.10.0-rc1"
+
+#endif // __COMMON_GIT_VERSION_HPP__
